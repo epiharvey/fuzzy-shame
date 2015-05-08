48c7b62956cb43dd1c7c19f27fc3f2830efb8354
epiharvey <iharvey@epiphan.com>
2015-05-08 09:29:11 -0400

    new NodeSpawner class

---
+++ src/main/java/com/epiphan/qa/grid/NodeSpawner.java
@@ -0,0 +1,21 @@
+package com.epiphan.qa.grid;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Properties;
+
+import org.apache.commons.exec.CommandLine;
+import org.apache.commons.exec.DefaultExecuteResultHandler;
+import org.apache.commons.exec.DefaultExecutor;
+import org.apache.commons.exec.ExecuteException;
+import org.apache.commons.exec.PumpStreamHandler;
+import org.apache.commons.exec.ShutdownHookProcessDestroyer;
+
+
+/**
+ * This is a class to spawn a given Jar by command line
+ * @author Ian Harvey [iharvey@epiphan.com]
+ */
+public class NodeSpawner {
+
+}
