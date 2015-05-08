ea3f6a3fe78e1271d129112a715bf701ff443ee5
epiharvey <iharvey@epiphan.com>
2015-05-08 13:16:23 -0400

    fleshed out NodeSpawner

--- grid/src/main/java/com/epiphan/qa/grid/NodeSpawner.java
+++ grid/src/main/java/com/epiphan/qa/grid/NodeSpawner.java
@@ -17,5 +17,82 @@ import org.apache.commons.exec.ShutdownHookProcessDestroyer;
  * @author Ian Harvey [iharvey@epiphan.com]
  */
 public class NodeSpawner {
-
+	
+	static class JarFileAttributes{
+		
+		private File filePath;
+		private String jarArgs;
+		
+		public File getFilePath(){
+			return filePath;
+		}
+		
+		public String getJarArgs(){
+			return jarArgs;
+		}
+		
+		@Override
+		public String toString(){
+			return "JarFileAttributes [filePath="+filePath.getAbsolutePath()+", jarArgs="+jarArgs+"]";
+		}
+		
+		public JarFileAttributes(File filePath, String jarArgs){
+			this.filePath = filePath;
+			this.jarArgs = jarArgs;
+		}
+	}
+	
+	public static void main(String[] args) throws ExecuteException, IOException, InterruptedException {
+		JarFileAttributes attributes = getJarFile(args);
+		Properties properties = new Properties();
+		
+		properties.load(NodeSpawner.class.getResourceAsStream("/spawner.properties"));
+		long interval = Long.parseLong((String) properties.get("defaultInterval"));
+		while(true){
+			continuouslyRestart(attributes, interval);
+			Thread.sleep(interval);
+			System.out.println("Application exited, respawning...");
+		}
+	}
+	
+	public static JarFileAttributes getJarFile(String[] args){
+		File file = null;
+		boolean wasJarFound = false;
+		int jarArgsIndex = 0;
+		StringBuffer sb = new StringBuffer();
+		for (int i = 0; i < args.length; i++){
+			if (args[i].contains(".jar")){
+				file = new File(args[i]);
+				wasJarFound = true;
+				jarArgsIndex = i+1;
+			}
+		}
+		if(!wasJarFound){
+			throw new RuntimeException("Please specify the jar file");
+		}
+		for (int i = jarArgsIndex; i < args.length; i++){
+			sb.append(args[i]).append(" ");
+		}
+		return new JarFileAttributes(file, sb.toString());
+	}
+	
+	public static void continuouslyRestart(JarFileAttributes attributes, long interval)
+			throws ExecuteException, IOException, InterruptedException {
+		System.out.println("Spawning the application");
+		CommandLine cmdLine = new CommandLine("java");
+		cmdLine.addArgument("-jar");
+		cmdLine.addArgument(attributes.getFilePath().getAbsolutePath()+" "+attributes.getJarArgs());
+		
+		DefaultExecutor executor = new DefaultExecutor();
+		executor.setStreamHandler(new PumpStreamHandler());
+		executor.setProcessDestroyer(new ShutdownHookProcessDestroyer());
+		DefaultExecuteResultHandler handler = new DefaultExecuteResultHandler();
+		while(!handler.hasResult()){
+			Thread.sleep(interval);
+		}
+		
+		if (handler.hasResult()){
+			ExecuteException e = handler.getException();
+		}
+	}
 }
