37d7a9be050f7801bbe28dd436e802c614684915
epiharvey <iharvey@epiphan.com>
2015-05-14 12:22:52 -0400

    Target IPs, users, passes, etc.. can now be spec'd by command line

--- tests/src/test/java/com/epiphan/qa/tests/DataProviders.java
+++ tests/src/test/java/com/epiphan/qa/tests/DataProviders.java
@@ -1,13 +1,14 @@
 package com.epiphan.qa.tests;
 
 import java.util.ArrayList;
+import java.util.Properties;
 
 import org.apache.commons.lang3.StringUtils;
 import org.testng.ITestContext;
 import org.testng.annotations.*;
 
 public class DataProviders {
-		
+
 	/* This should be the only important thing in this class.
 	 * Builds the TestEnvironments to package data for the @Test methods in other test classes which extend
 	 * SeleniumTest.
@@ -16,28 +17,33 @@ public class DataProviders {
 	 * TODO Make this optionally handle auth cridentials on a per-target basis.
 	 */
 	@DataProvider(name="env", parallel=true)
-	public static Object[][] env(ITestContext context){
-		
+	public static Object[][] env(){
+
 		ArrayList<Object[]> envs = new ArrayList<Object[]>();
-		
-		String hubIP = context.getCurrentXmlTest().getParameter("hubIP");
-		String[] targetIP = StringUtils.split(context.getCurrentXmlTest().getParameter("targetIP"), ' ');
-		String targetUser = context.getCurrentXmlTest().getParameter("targetUser");
-		String targetPass = context.getCurrentXmlTest().getParameter("targetPass");
-		String[] browser = StringUtils.split(context.getCurrentXmlTest().getParameter("browser"), ' ');
-		
+
+		Properties p = System.getProperties();
+		String hubIP = p.getProperty("hubIP");
+		String[] targetIP = StringUtils.split(p.getProperty("targetIP"), ",");
+		String targetUser = p.getProperty("targetUser");
+		String targetPass = p.getProperty("targetPass");
+		String[] browser = StringUtils.split(p.getProperty("browser"), ",");
+
 		for(String ip : targetIP){
 			for(String bsr : browser){
 				Object[] o = {new TestEnvironment(hubIP, ip, targetUser, targetPass, bsr)};
 				envs.add(o);
-				//TestEnvironment env = new TestEnvironment(hubIP, targetIP, targetUser, targetPass, browser);
 			}
 		}
-		
+
 		Object[][] ret = new Object[envs.size()][];
 		envs.toArray(ret);
 		return ret;
 	}
-	
-	
+
+	@Test
+	public void selfTest(){
+		DataProviders.env();
+	}
+
+
 }
