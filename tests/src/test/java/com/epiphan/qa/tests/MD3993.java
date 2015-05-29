ecfac9acdd8531936b9828dadc085b745dbb605e
epiharvey <iharvey@epiphan.com>
2015-05-29 09:49:21 -0400

    Fixed chromedriver on mac by using javascript injection.

--- tests/src/test/java/com/epiphan/qa/tests/MD3993.java
+++ tests/src/test/java/com/epiphan/qa/tests/MD3993.java
@@ -4,9 +4,14 @@ import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 import org.apache.commons.lang3.StringUtils;
+import org.openqa.selenium.Alert;
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.security.UserAndPassword;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.support.ui.WebDriverWait;
@@ -34,8 +39,9 @@ public class MD3993 {
 		
 		Reporter.log("Log in as: "+env.targetUser+":"+env.targetPass);
 		driver.get("http://"+env.targetUser+":"+env.targetPass+"@"+env.targetIP+"/admin/infocfg");
-		
 		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		
+		Actions actions = new Actions(driver);
 		//Go to channel config
 	    el = driver.findElement(By.linkText("Add channel"));
 	    Assert.assertNotNull(el, "Could not add channel. Button missing?");
@@ -47,9 +53,13 @@ public class MD3993 {
 	    driver.findElement(By.name("value")).clear();
 	    driver.findElement(By.name("value")).sendKeys("MD-3993\n");
 	    driver.findElement(By.cssSelector("[data-ember-action=\"376\"]")).click();
-	    new WebDriverWait(driver, 10).until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector(".ember-view.new-source-button")));
-	    driver.findElement(By.cssSelector(".ember-view.new-source-button")).click();
-	    driver.findElement(By.cssSelector("a.select2-choice")).click();
+	    actions.sendKeys(Keys.PAGE_DOWN);
+	    new WebDriverWait(driver, 10).until(ExpectedConditions.visibilityOfElementLocated(By.id("ember486")));
+	    el = driver.findElement(By.cssSelector("div#ember486.ember-view.new-source-button"));
+	    JavascriptExecutor ex = (JavascriptExecutor) driver;
+	    ex.executeScript("arguments[0].click();", el);
+	    //new WebDriverWait(driver, 5).until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[contains(text(), 'Picture')]")));
+	    driver.findElement(By.xpath("//*[contains(text(), 'Picture')]")).click();
 	    driver.findElement(By.cssSelector("ul.select2-results li:first-child")).click();
 	    driver.findElement(By.cssSelector(".button.save-button")).click();
 	    Reporter.log("Channel successfully created");
